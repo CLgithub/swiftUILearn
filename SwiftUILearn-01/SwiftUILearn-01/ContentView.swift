eee5d926e2fed2424b3975840c3b0884ab8d76bb
leichen <leichen070@gmail.com>
2024-06-25 20:52:38 +0800

    app1-3

--- SwiftUILearn-01/SwiftUILearn-01/ContentView.swift
+++ SwiftUILearn-01/SwiftUILearn-01/ContentView.swift
@@ -10,11 +10,18 @@ import SwiftUI // 导入 SwiftUI 框架
 struct ContentView: View {
     // 定义一个计算属性 body，它返回一些视图内容
     var body: some View {
-        VStack { // 使用 VStack 将视图垂直排列 HStack 水平排列
-            Image(systemName: "globe") // 添加一个系统图标，图标名称为 "globe"
-                .imageScale(.large) // 设置图标的尺寸为大
-                .foregroundStyle(.tint) // 设置图标的前景样式为 tint
-            Text("Hello,  SwiftUI!") // 添加一个文本视图，显示 "Hello, world!"
+        VStack(alignment: .leading) { // 使用 VStack 将视图垂直排列 HStack 水平排列
+//            Image(systemName: "globe") // 添加一个系统图标，图标名称为 "globe"
+//                .imageScale(.large) // 设置图标的尺寸为大
+//                .foregroundStyle(.tint) // 设置图标的前景样式为 tint
+            Text("Turtle Rock")// 添加一个文本视图，显示 "Hello, world!"
+                .font(.title)   // 设置文本字体
+                .foregroundColor(.black)    // 设置文本颜色
+            HStack { // 内部嵌套一个水平布局
+                Text("Joshua Tree National Park").font(.subheadline)
+                Spacer()    // 添加空间
+                Text("California").font(.subheadline)
+            }
         }
         .padding() // 给 VStack 添加填充（padding）
     }
