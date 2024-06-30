a00555ee8563e3dd5e5f512c9e5243aaf7af9321
leichen <leichen070@gmail.com>
2024-06-30 17:01:30 +0800

    为每个地标创建收藏按钮

---
+++ SwiftUILearn-01/SwiftUILearn-01/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  SwiftUILearn-01
+//  收藏按钮
+//  Created by L on 2024/6/30.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet:Bool // 这是一个属性包装器，用于创建对某个值的引用，而不是值本身。它允许子视图修改父视图中的状态，实现了双向数据流。
+    // 绑定属性包装器可让您在存储数据的属性和显示和更改数据的视图之间进行读写。由于您使用绑定，因此在此视图内所做的更改会传播回数据源。
+    
+    var body: some View {
+        Button{ // 添加一个按钮
+            isSet.toggle()  // 按钮用来切换isSet
+        } label:{
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill": "star") // 按钮名称Toggle Favorite，用一个系统图标，若isSet=true，就显示填满的五角星
+                .labelStyle(.iconOnly) 
+                .foregroundStyle(isSet ? .yellow: .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- SwiftUILearn-01/SwiftUILearn-01/Views/Landmarks/LandmarkDetail.swift
+++ SwiftUILearn-01/SwiftUILearn-01/Views/Landmarks/LandmarkDetail.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-    
+    @Environment(ModelData.self) var modelData // 从环境中获取modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int{
+        // firstIndex(where:) 是数组的一个方法，用于找到第一个满足指定条件的元素的索引。$0.id == landmark.id 是查找条件，即找到第一个 id 与 landmark.id 相同的元素。
+        // 是一个闭包（closure），用于指定查找条件
+        // ! 强制解包（force unwrap），表明我们确信一定能找到符合条件的地标，所以返回的索引不是可选值（optional）
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!    // 找到landmarks数组中第一个id与landmark.id 相同的元素的索引
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData // 定义一个bindable变量 模型数据与视图之间进行双向数据绑定 简化和自动化数据变化通知
         ScrollView{ // 总的嵌套 垂直布局VStack 改为 ScrollView 便于滚动查看
             MapView(coordinate: landmark.locationCoordinate).frame(height: 300)    // 添加地图视图，高300
             
@@ -21,9 +29,13 @@ struct LandmarkDetail: View {
     //            Image(systemName: "globe") // 添加一个系统图标，图标名称为 "globe"
     //                .imageScale(.large) // 设置图标的尺寸为大
     //                .foregroundStyle(.tint) // 设置图标的前景样式为 tint
-                Text(landmark.name)// 添加一个文本视图，显示 "Hello, world!"
-                    .font(.title)   // 设置文本字体
-                    .foregroundColor(.black)    // 设置文本颜色
+                HStack{
+                    Text(landmark.name)// 添加一个文本视图，显示 "Hello, world!"
+                        .font(.title)   // 设置文本字体
+                        .foregroundColor(.black)    // 设置文本颜色
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+               
                 HStack { // 内部嵌套一个水平布局
                     Text(landmark.park)
                     Spacer()    // 添加空间
