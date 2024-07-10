317ef17344df851c09cc64b36c15e5ac002369ef
leichen <leichen070@gmail.com>
2024-07-10 22:11:57 +0800

    将徒步数据添加到应用程序

---
+++ SwiftUILearn-01/SwiftUILearn-01/Model/Hike.swift
@@ -0,0 +1,31 @@
+//
+//  Hike.swift
+//  SwiftUILearn-01
+//
+//  Created by L on 2024/7/10.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String    // 名称
+    var id: Int         // id
+    var distance: Double    // 距离
+    var difficulty: Int     // 难度系数
+    var observations: [Observation] //
+
+    struct Observation: Codable, Hashable {
+        var elevation: Range<Double>    // 海拔范围
+        var pace: Range<Double>         // 步幅范围
+        var heartRate: Range<Double>    // 每分钟心跳次数范围
+        var distanceFromStart: Double   // 距起点距离
+    }
+    
+    // 创建一个格式化工具实例
+    static var formatter = LengthFormatter()    // LengthFormatter 是 Foundation 框架中的一个类，用于格式化长度值
+   
+    // 这是一个计算属性 distanceText，它返回一个格式化后的字符串
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer) // 返回格式化后的fromValue: distance, unit: .kilometer string(fromValue:unit:) 方法将 distance 值转换为字符串，单位指定为千米。
+    }
+}
--- SwiftUILearn-01/SwiftUILearn-01/Model/ModelData.swift
+++ SwiftUILearn-01/SwiftUILearn-01/Model/ModelData.swift
@@ -11,6 +11,7 @@ import Foundation
 class ModelData{
     // 定义一个Landmark类型的数组 landmarks，数组值来源于调用 load方法，方法传参String landmarkData.json
     var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike]=load("hikeData.json")
 }
 
 // 定义一个泛型方法，方法名：load，用于从指定的文件中加载并解码数据，泛型类型 T 必须符合 Decodable 协议
