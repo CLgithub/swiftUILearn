e39b04dcdb75ff97c9e370f104edcabb43615c0d
leichen <leichen070@gmail.com>
2024-08-03 16:11:29 +0800

    添加多视图切换显示

--- SwiftUILearn-01/SwiftUILearn-01/Views/ContentView.swift
+++ SwiftUILearn-01/SwiftUILearn-01/Views/ContentView.swift
@@ -8,12 +8,30 @@ import SwiftUI // 导入 SwiftUI 框架
 
 // 定义 ContentView 结构体，遵循 View 协议
 struct ContentView: View {
+    
+    @State private var selectedTab = 0
+    
     // 定义一个计算属性 body，它返回一些视图内容
     var body: some View {
-        Badge() // 徽章
-        LandmarkList()  // 列表视图
-        HikeView(hike: ModelData().hikes[0]) .padding() // 动画视图
-//        Spacer()
+        TabView(selection: $selectedTab) {
+            LandmarkList()  // 列表显示景点
+                .tabItem {
+                    Label("Landmarks", systemImage: "list.bullet")
+                }
+                .tag(0)
+            
+            Badge()     // 徽章
+                .tabItem {
+                    Label("Badge", systemImage: "shield")
+                }
+                .tag(1)
+            HikeView(hike: ModelData().hikes[0])    // 动画
+                .padding()
+                .tabItem {
+                    Label("Hike", systemImage: "figure.hiking")
+                }
+                .tag(2)
+        }
     }
 }
 
